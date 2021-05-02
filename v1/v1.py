1ea8c6f2ab010b3d79cbd92c5a9507d989dfa28a
James Martin <jjamesmartiin@gmail.com>
2021-05-02 00:18:12 -0700

    twitter_api: refined logic for buying dogecoin

--- v1/v1.py
+++ v1/v1.py
@@ -1,4 +1,5 @@
 import tweepy as tp  # pip install tweepy
+import re # pip install regex
 
 auth = tp.OAuthHandler("petU9I2MLe9koGOcikP8DN2WS", "cBVqJTcDTpk0F6OCqL7G8px2dgniYVPbEuvVXDMQVK8LWk6BlJ")
 auth.set_access_token("1267671961846439938-nMzYeeMo49SRMC0JUV2kdlLAPMpFwV", "5S4w4f7mM7LoGANKZFkLkQAo284TaxDt4z88LHca3rCTM")
@@ -8,10 +9,15 @@ api = tp.API(auth)
 user = api.get_user('@elonmusk')
 timeline = api.user_timeline('@elonmusk', trim_user=True, exclude_replies=True)
 
+doge = "space"
+
+dogecoin = "spacex."
+
 for tweet in timeline:
     text = tweet.text.lower()
-    if 'snl' not in text:
-        print('buy dogecoin $50')
-    elif 'dogecoin' in text:
+    print(text + ': ')
+    # print(re.search(doge, text))
+    if re.search(dogecoin, text):
         print('buy dogecoin $100')
-    print('test')
+    elif re.search(doge, text):
+        print(' buy dogecoin $50')
